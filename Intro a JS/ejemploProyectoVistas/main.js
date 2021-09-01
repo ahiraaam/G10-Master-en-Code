360071baa9a829206ed2407de2e45a0d7a1c33ea
ahiraaam <ahicruma23@gmail.com>
2021-08-31 21:32:29 -0500

    Funcion de ingresar

--- Intro a JS/ejemploProyectoVistas/main.js
+++ Intro a JS/ejemploProyectoVistas/main.js
@@ -1,25 +1,35 @@
 let btnLogin = document.getElementById("btn-login");
+let btnIngresar = document.getElementById("btn-ingresar");
 let seccionLogin = document.getElementById("seccionLogin");
 let seccionPrincipal = document.getElementById("seccionPrincipal");
+let seccionIngresarMonto = document.getElementById("seccionIngresarMonto");
 //Ocultando sección principal
 seccionPrincipal.hidden = true;
+seccionIngresarMonto.hidden = true;
 var usuarios = [
   {
     name: "Carlos",
     usuario: "cburguenog@gmail.com",
     pass: "contrasena123",
+    saldo: 10,
   },
   {
     name: "jorge",
     usuario: "jorge.mendoza@gmail.com",
     pass: "jorge123",
+    saldo: 10,
+  },
+  {
+    name: "usuario",
+    usuario: "usuario",
+    pass: "123",
+    saldo: 10,
   },
 ];
-
+let usuarioActual;
 function login(correo, password) {
   let flagAlert = true;
   let alert = document.getElementById("liveAlert");
-
   for (let i = 0; i < usuarios.length; i++) {
     let tmpUsuario = usuarios[i];
     //Esta es la validacio para ver si si existe el usuario
@@ -27,11 +37,13 @@ function login(correo, password) {
       correo.toLowerCase() === tmpUsuario.usuario.toLowerCase() &&
       password === tmpUsuario.pass
     ) {
-      //flagAlert = false;
+      flagAlert = false;
       //Oculto la sección de login (el formulario)
       seccionLogin.hidden = true;
       //Muestro la sección principal (los botones)
       seccionPrincipal.hidden = false;
+      usuarioActual = tmpUsuario;
+      console.log(usuarioActual);
       break;
     }
   }
@@ -41,7 +53,21 @@ function login(correo, password) {
     alert.textContent = "Upps!! Revisa usuario y contraseña!!";
   }
 }
-
+function ingresar(ingreso) {
+  if (usuarioActual.saldo + ingreso > 990) {
+    alert("Tu saldo no puede ser mayor a 990");
+  } else {
+    //usuarioActual.saldo+=ingreso
+    usuarioActual.saldo = usuarioActual.saldo + ingreso;
+    let pSaldoIngresado = document.getElementById("p-saldo-ingresado");
+    let pNuevoSaldo = document.getElementById("p-nuevo-saldo");
+    pSaldoIngresado.innerText = "El saldo que ingresaste es: " + ingreso;
+    pNuevoSaldo.innerText = "Tu nuevo saldo es " + usuarioActual.saldo;
+    /*
+    nuevoSaldo = ingreso + usuarioActual.saldo
+    usuarioActual.saldo = nuevoSaldo*/
+  }
+}
 //Detecto el click en el botón
 btnLogin.addEventListener("click", function () {
   //Obtengo valores de usuario y contraseña
@@ -51,4 +77,15 @@ btnLogin.addEventListener("click", function () {
   //Se ejecuta la función login
   //Adentro de la función login es donde oculto/muestro secciones
   login(usuario, password);
+});
+
+// PARA INGRESAR MONTO
+btnIngresar.addEventListener("click", function () {
+  seccionIngresarMonto.hidden = false;
+});
+
+let btnAceptarIngreso = document.getElementById("btn-aceptar-ingreso");
+btnAceptarIngreso.addEventListener("click", function () {
+  let ingreso = parseInt(document.getElementById("input-ingreso").value);
+  ingresar(ingreso);
 });
