7245b353ce0f4dd1c2a89c4cf4d8a0d943050104
Carlos Burgueño <cburguenog@gmail.com>
2021-09-15 20:35:36 -0600

    Ejercicio 1 Pilas/Colas

---
+++ Computer Science/Ejercicios/ejerciciosColasPilas.js
@@ -0,0 +1,34 @@
+/*
+1.- Hacer una función que reciba como parámetros una pila, y un número.
+La función debe hacer “pop” de tantos elementos como diga el número (segundo parámetro).
+Entrada : mifuncion([‘Manzana’,‘Cebolla’,‘Apio’,‘Naranja’,‘Papaya’,‘Sandía’,‘Melón’],4)
+Salida: [‘Naranja’,‘Papaya’,‘Sandía’,‘Melón’]
+*/
+function multiPop( miPila, num  ){
+    for( let i=0; i < num; i++ )
+    {
+        console.log(miPila.peek());
+        miPila.pop();
+    }
+}
+
+const pilaFrutas = new Pila();
+pilaFrutas.push("Manzana");
+pilaFrutas.push("Cebolla");
+pilaFrutas.push("Apio");
+pilaFrutas.push("Naranja");
+pilaFrutas.push("Papaya");
+pilaFrutas.push("Sandía");
+pilaFrutas.push("Melón");
+
+console.log(pilaFrutas); // 7
+setTimeout(()=>{
+    multiPop(pilaFrutas,4);
+    console.log(pilaFrutas); // 3
+},5000)
+
+
+/*2.- Escribe una función “reemplazar” que tenga como parámetros una pila de elementos de tipo Number, y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún lugar de la pila, sea reemplazado por el primero (Solo la primera vez), y hará pop de los siguientes elementos.
+Entrada: reemplazar([3,2,3,4,6,8,1,2,5,5], 7, 2)
+Salida: [3,2,3,4,6,8,1,7]
+*/
--- Computer Science/Pilas/pilas_con_arrays.js
+++ Computer Science/Pilas/pilas_con_arrays.js
@@ -26,6 +26,7 @@ class Pila {
   }
 }
 
+/*
 const pila1 = new Pila();
 pila1.push("Libro 1"); // ["Libro1"]
 pila1.push("Libro 2"); // ["Libro1", "Libro2"]
@@ -33,8 +34,10 @@ pila1.push("Libro 3"); // ["Libro1", "Libro2", "Libro3"]
 pila1.push("Libro 4"); // ["Libro1", "Libro2", "Libro3", "Libro4"]
 /*setTimeout(() => {
   pila1.pop();
-}, 3000);*/
+}, 3000);
 console.log("Pop", pila1.pop()); //  "Libro4"
 console.log(pila1); // ["Libro1", "Libro2", "Libro3"]
 console.log("Peek", pila1.peek()); // "Libro3"
 pila1.print(); // ["Libro1", "Libro2", "Libro3"]
+
+*/
