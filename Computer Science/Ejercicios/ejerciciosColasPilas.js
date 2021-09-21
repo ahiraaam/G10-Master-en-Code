2c175f3f44d6281d5e95e1ea4b922f343fa15aea
Carlos Burgueño <cburguenog@gmail.com>
2021-09-21 17:48:48 -0600

    respaldando cambios locales

--- Computer Science/Ejercicios/ejerciciosColasPilas.js
+++ Computer Science/Ejercicios/ejerciciosColasPilas.js
@@ -28,7 +28,21 @@ setTimeout(()=>{
 },5000)
 
 
-/*2.- Escribe una función “reemplazar” que tenga como parámetros una pila de elementos de tipo Number, y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún lugar de la pila, sea reemplazado por el primero (Solo la primera vez), y hará pop de los siguientes elementos.
+/*2.- Escribe una función “reemplazar” que tenga como parámetros una pila de elementos de tipo Number, 
+y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún 
+lugar de la pila, sea reemplazado por el primero (Solo la primera vez), y hará pop de los siguientes elementos.
 Entrada: reemplazar([3,2,3,4,6,8,1,2,5,5], 7, 2)
 Salida: [3,2,3,4,6,8,1,7]
 */
+
+function reemplazar(miPila, nuevo,viejo)
+{
+    const auxPila = new Pila();
+    for(let i=0; i < miPila.size(); i++)
+    {
+        if ( miPila.peek() === viejo )
+        {
+            
+        }
+    }
+}
