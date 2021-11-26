d9b6e98eae0dca0ecce0f0a7519089cb26aef86d
ahiraaam <ahicruma23@gmail.com>
2021-11-25 20:13:42 -0600

    Navbar

--- FrontendJS/cripto-deploy/src/components/Navbar/Navbar.js
+++ FrontendJS/cripto-deploy/src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
-
 const Navbar = () => {
   return (
     <>
@@ -21,7 +20,7 @@ const Navbar = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/noticis">Noticias</Link>
+              <Link to="/noticias">Noticias</Link>
             </li>
             <li className="services">
               <Link to="/">Servicios</Link>
--- FrontendJS/cripto-deploy/src/routes/index.js
+++ FrontendJS/cripto-deploy/src/routes/index.js
@@ -4,11 +4,13 @@ import Home from "../pages/Home/Home";
 import Contacto from "../pages/Contacto/Contacto";
 import Noticias from "../pages/Noticias/Noticias";
 import Error404 from "../pages/Error404/Error404";
+import Navbar from "../components/Navbar/Navbar";
 
 const Routes = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/noticias" component={Noticias} />
