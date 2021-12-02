4f9d4a06b9f1649d959b5b80d4fc8bf15c63a0ef
ahiraaam <ahicruma23@gmail.com>
2021-12-01 21:30:53 -0600

    UseContext

--- FrontendJS/context/src/components/ListUsers/ListUsers.js
+++ FrontendJS/context/src/components/ListUsers/ListUsers.js
@@ -2,8 +2,6 @@ import React from "react";
 import { useUserContext } from "../../context/UserContext";
 export default function ListUsers() {
   const context = useUserContext();
-  //Tenemos "guardado" : listUsers ,usuarioActual, setUsuarioActual
-
   return (
     <div>
       {context.listaUsuarios.map((user) => (
